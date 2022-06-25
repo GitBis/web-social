3644bcaa8924ec7687aa8d9cafaa490ca96e2375
GitBis <00370819@uca.edu.sv>
2022-06-25 02:21:09 -0600

    Pagina en React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component{
+
+  render(){
+    return(
+     <body>
+     <text>Web Social</text>
+     <img src="/wallpaper_game.png" alt=""/></body>
+    );
+  }
+}
+
+export default App;
